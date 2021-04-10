2c52c1ae1b9ecf820ac7c236408deccec8975983
priyanka <priyankachadar1105@gmail.com>
2021-04-10 16:16:25 +0530

    Auth Reducer

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 import { setAlert } from "./alert";
+import{
+    REGISTER_SUCCESS,
+    REGISTER_FAIL
+}from './types';
 
-export const register = ({ name, email, password }) => async (dispatch) => {
+
+//Regisster User
+export const register = ({ name, email, password }) => async dispatch => {
   const config = {
     headers: {
-      "Content-Type": "application/json",
-    },
+      'Content-Type': 'application/json'
+    }
   };
 
   const body = JSON.stringify({ name, email, password });
@@ -14,18 +20,20 @@ export const register = ({ name, email, password }) => async (dispatch) => {
     const res = await axios.post('/api/user', body, config);
 
     dispatch({
-      type: "REGISTER_SUCCESS",
+      type: REGISTER_SUCCESS,
       payload: res.data,
 
     });
-    console.log("from auth action: ", res);
-  } catch (error) {
-    const errors = error.response.data.errors;
+
+  } catch (err) {
+    const errors = err.response.data.errors;
 
     if (errors) {
-      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
     }
 
-    dispatch({ type: "REGISTER_FAIL" });
+    dispatch({ 
+        type: REGISTER_FAIL
+     });
   }
 };
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -30,8 +30,8 @@ export default function authReducer(state = initialState, action){
             token: null,
             isAuthenticated: false,
             loading: false
-          }; 
+          }
           default:
               return state;
     }
-};
+}
