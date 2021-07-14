66e5c2428e2ab24e7491ec93745b29e497d5c977
priyanka <priyankachadar1105@gmail.com>
2021-07-14 16:36:48 +0530

    Deployment

--- server.js
+++ server.js
@@ -15,8 +15,9 @@ app.use(express.json())
 //Define routes
 app.use('/api/user', require('./routes/api/user'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/post', require('./routes/api/post'));
 app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
+
 
 // Serve static assets in production
 if(process.env.NODE_ENV === 'production') {
